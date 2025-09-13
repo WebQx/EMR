0622b416dde21755900852ec937538c7127fa8e4
Taha Ahmad <webqx2103@gmail.com>
2025-09-13 17:49:42 +0000

    Update frontend API URLs and add Nginx config for public EMR/MariaDB integration

--- webqx-emr-system/api/webqx-api.php
+++ webqx-emr-system/api/webqx-api.php
@@ -5,7 +5,7 @@
  */
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://webqx.github.io');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
